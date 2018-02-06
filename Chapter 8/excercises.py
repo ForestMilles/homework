612165c92e6008292db5e4e65bdd2594596d0183
ForestMilles <31958674+ForestMilles@users.noreply.github.com>
2018-02-06 14:17:55 -0500

    changes

--- Chapter 8/excercises.py
+++ Chapter 8/excercises.py
@@ -1,3 +1,25 @@
-fruit = input("type a fruit")
+def find2(string, ch, start=0, end=None):
+    ix = start
+    if end is None:
+        end = len(string)
+    while ix < len(string):
+        if string[ix] == ch:
+            return ix
+        ix += 1
+    return -1
 
-print(len(fruit))
+
+print (find2("banana", "a", 3, 4))
+
+
+
+def count_letters(text, letter):
+    count = 0
+    for c in text:
+        if c == letter:
+            count += 1
+    return count
+
+
+print(count_letters("banana", "n") == 3)
+test(count_letters("banana", "b") == 1)
