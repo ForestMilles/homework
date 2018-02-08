9a91b4faf84e84a8f2a184c9b3a55ed647db81ac
ForestMilles <31958674+ForestMilles@users.noreply.github.com>
2018-02-08 08:26:45 -0500

    changes to syntax error in test for #9

--- Chapter 8/chapter8hw.py
+++ Chapter 8/chapter8hw.py
@@ -29,6 +29,8 @@ def test_suite():
     test(remove_letter("a", "banana") == "bnn")
     test(remove_letter("z", "banana") == "banana")
     test(remove_letter("i", "Mississippi") == "Msssspp")
+    test(remove_letter("b", "") == "")
+    test(remove_letter("b", "c") == "c")
 
 
 """ Modify:
