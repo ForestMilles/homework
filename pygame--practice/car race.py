0248ac97f2263849858cf05cf1059e1c7a4b725a
ForestMilles <31958674+ForestMilles@users.noreply.github.com>
2018-02-15 19:39:07 -0500

    reverted back to step 8 as we r supposed to stop at step 10, but once stopping at step ten cant access game b/c doesn't have interactive buttons yet
    also my computer was having some battery issues and git hub stated application failed (i had to sign in again) which is why the pushes to gthub r so close.

--- pygame--practice/car race.py
+++ pygame--practice/car race.py
@@ -11,8 +11,6 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 
-block_color = (53, 115, 255)
-
 car_width = 73
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
@@ -20,12 +18,6 @@ pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
 
 carImg = pygame.image.load('racecar.png')
-
-
-def things_dodged(count):
-    font = pygame.font.SysFont(None, 25)
-    text = font.render("Dodged: " + str(count), True, black)
-    gameDisplay.blit(text, (0, 0))
 
 
 def things(thingx, thingy, thingw, thingh, color):
@@ -58,25 +50,6 @@ def crash():
     message_display('You Crashed')
 
 
-def game_intro():
-    intro = True
-
-    while intro:
-        for event in pygame.event.get():
-            print(event)
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-        gameDisplay.fill(white)
-        largeText = pygame.font.Font('freesansbold.ttf', 115)
-        TextSurf, TextRect = text_objects("A bit Racey", largeText)
-        TextRect.center = ((display_width / 2), (display_height / 2))
-        gameDisplay.blit(TextSurf, TextRect)
-        pygame.display.update()
-        clock.tick(15)
-
-
 def game_loop():
     x = (display_width * 0.45)
     y = (display_height * 0.8)
@@ -85,13 +58,9 @@ def game_loop():
 
     thing_startx = random.randrange(0, display_width)
     thing_starty = -600
-    thing_speed = 4
+    thing_speed = 7
     thing_width = 100
     thing_height = 100
-
-    thingCount = 1
-
-    dodged = 0
 
     gameExit = False
 
@@ -115,11 +84,10 @@ def game_loop():
         x += x_change
         gameDisplay.fill(white)
 
-        things(thing_startx, thing_starty, thing_width, thing_height, block_color)
-
+        # things(thingx, thingy, thingw, thingh, color)
+        things(thing_startx, thing_starty, thing_width, thing_height, black)
         thing_starty += thing_speed
         car(x, y)
-        things_dodged(dodged)
 
         if x > display_width - car_width or x < 0:
             crash()
@@ -127,22 +95,20 @@ def game_loop():
         if thing_starty > display_height:
             thing_starty = 0 - thing_height
             thing_startx = random.randrange(0, display_width)
-            dodged += 1
-            thing_speed += 1
-            thing_width += (dodged * 1.2)
 
+        ####
         if y < thing_starty + thing_height:
             print('y crossover')
 
             if x > thing_startx and x < thing_startx + thing_width or x + car_width > thing_startx and x + car_width < thing_startx + thing_width:
                 print('x crossover')
                 crash()
+        ####
 
         pygame.display.update()
         clock.tick(60)
 
 
-game_intro()
 game_loop()
 pygame.quit()
 quit()
