86890e927fce6c73923ed4a541632959a203df70
ForestMilles <31958674+ForestMilles@users.noreply.github.com>
2018-02-25 18:05:00 -0500

    part 1 pygame buttons

--- pygame--practice/car race.py
+++ pygame--practice/car race.py
@@ -58,6 +58,29 @@ def crash():
     message_display('You Crashed')
 
 
+def game_intro():
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf', 115)
+        TextSurf, TextRect = text_objects("A bit Racey", largeText)
+        TextRect.center = ((display_width / 2), (display_height / 2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        pygame.draw.rect(gameDisplay, green, (150, 450, 100, 50))
+        pygame.draw.rect(gameDisplay, red, (550, 450, 100, 50))
+
+        pygame.display.update()
+        clock.tick(15)
+
+
 def game_loop():
     x = (display_width * 0.45)
     y = (display_height * 0.8)
--- pygame--practice/shape.py
+++ pygame--practice/shape.py
@@ -16,7 +16,7 @@ pixAr = pygame.PixelArray(gameDisplay)
 pixAr[10][20] = green
 pygame.draw.line(gameDisplay, blue, (100,200), (300,500),5)
 pygame.draw.rect(gameDisplay, red, (400,200,50,25))
-pygame.draw.circle(gameDisplay, white, (400,400), 100, 50)
+pygame.draw.circle(gameDisplay, white, (400,400), 100, 50 )
 pygame.draw.polygon(gameDisplay, green, ((55,75),(36,124),(260,375),(450,25),(6,540)))
 
 print("")
