91c82594e8938ddf5e2b181a8df62677bd981c52
ForestMilles <31958674+ForestMilles@users.noreply.github.com>
2018-04-05 14:20:04 -0400

    update

--- SpringBreakHw/chapter10.py
+++ SpringBreakHw/chapter10.py
@@ -75,7 +75,7 @@ wn.listen()
 wn.mainloop()
 
 """2. Change the traffic light program so that changes occur automatically, driven by a timer."""
-import turtle  # Tess becomes a traffic light.
+import turtle  
 
 wn = turtle.Screen()
 wn.title("Tess becomes a traffic light!")
@@ -123,11 +123,11 @@ draw_housing()
 draw_housing2()
 
 tess.penup()
-# Position tess onto the place where the green light should be
+
 tess.forward(40)
 tess.left(90)
 tess.forward(50)
-# Turn tess into a big green circle
+
 tess.shape("circle")
 tess.shapesize(3)
 tess.fillcolor("green")
@@ -157,27 +157,22 @@ hunt.shape('circle')
 hunt.shapesize(3)
 hunt.fillcolor('gray')
 
-# A traffic light is a kind of state machine with three states,
-# Green, Orange, Red.  We number these states  0, 1, 2
-# When the machine changes state, we change tess' position and
-# her fillcolor.
 
-# This variable holds the current state of the machine
 state_num = 0
 state_num_1 = 0
 
 
 def timer():
     global state_num
-    if state_num == 0:  # Transition from state 0 to state 1
+    if state_num == 0:
         tess.forward(70)
         tess.fillcolor("orange")
         state_num = 1
-    elif state_num == 1:  # Transition from state 1 to state 2
+    elif state_num == 1:
         tess.forward(70)
         tess.fillcolor("red")
         state_num = 2
-    else:  # Transition from state 2 to state 0
+    else:
         tess.back(140)
         tess.fillcolor("green")
         state_num = 0
@@ -186,11 +181,11 @@ def timer():
 
 def timer2():
     global state_num_1
-    if state_num_1 == 0:  # Transition from state 0 to state 1
+    if state_num_1 == 0:
         guest.fillcolor('gray')
         yay.fillcolor('green')
         state_num_1 = 1
-    elif state_num_1 == 1:  # Transition from state 1 to state 2
+    elif state_num_1 == 1:
         yay.fillcolor('gray')
         bob.fillcolor('green')
         hunt.fillcolor('orange')
