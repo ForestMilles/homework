10341aad959eecfb71b2d65964aecae6684288a5
ForestMilles <31958674+ForestMilles@users.noreply.github.com>
2018-05-09 13:40:08 -0400

    update

--- chapter 15/practice H_1.py
+++ chapter 15/practice H_1.py
@@ -1,48 +1,38 @@
 class Point:
     """ Point class represents and manipulates x,y coords. """
 
-    def __init__(self):
+    def __init__(self, x=0, y=0):
         """ Create a new point at the origin """
-        self.x = 0
-        self.y = 0
-
-p = Point()         # Instantiate an object of type Point
-q = Point()         # Make a second point
-
-print(p.x, p.y, q.x, q.y)  # Each point object has its own x and y
-
-print("(x={0}, y={1})".format(p.x, p.y))
-
-distance_squared_from_origin = p.x * p.x + p.y * p.y
-
-p = Point()
-p.x = 7
-p.y = 6
-
-class Point:
-    """ Point class represents and manipulates x,y coords. """
-
-    def __init__(self, x=0, y=0):
-        """ Create a new point at x, y """
-        self.x = x
-        self.y = y
-
-# Other statements outside the class continue below here.
-
-p = Point(4, 2)
->>> q = Point(6, 3)
->>> r = Point()       # r represents the origin (0, 0)
->>> print(p.x, q.y, r.x)
-4 3 0
-
-class Point:
-    """ Create a new Point, at coordinates x, y """
-
-    def __init__(self, x=0, y=0):
-        """ Create a new point at x, y """
         self.x = x
         self.y = y
 
     def distance_from_origin(self):
         """ Compute my distance from the origin """
         return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+    def __str__(self):  # All we have done is renamed the method
+        return "({0}, {1})".format(self.x, self.y)
+
+p = Point(3,4)
+q = Point(10,11)
+f = Point(3,4)
+
+print(p.x,p.y,q.x,q.y)
+p.x = 4
+p.y = 6
+q.x = 10
+q.y = 11
+print(p.x, p.y, q.x, q.y)
+
+print("(x={0}, y={1})".format(p.x, p.y))
+distance_squared_from_origin = p.x * p.x + p.y * p.y
+dist = q.distance_from_origin()
+print(dist)
+
+def print_point(pt):
+    print("({0}, {1})".format(pt.x, pt.y))
+
+print_point(p)
+
+#print(p.to_string())
+print(str(p))
